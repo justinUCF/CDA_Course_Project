2d5b265c850b47d06306bebc7e347fed41919e30
Justin <gamboajustin03@gmail.com>
2024-04-19 15:39:17 -0400

    Update 1.0

--- project.c
+++ project.c
@@ -28,13 +28,28 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-
+    int lowerLim = 0; // 0x0000 = 0
+    int upperLim = 65535; // 0xFFFF = 65535
+    if(PC < lowerLim || PC > upperLim || PC%4 != 0){
+        return 1;
+    }
+    else{
+        *instruction = Mem[PC];
+        return 0;
+    }
 }
 
 /* Instruction Partition */
 /* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
+    *op = (instruction & 0xFC000000) >> 26;
+    *r1 = (instruction & 0x3E00000) >> 21;
+    *r2 = (instruction & 0x1F0000) >> 16;
+    *r3 = (instruction & 0xF800) >> 11;
+    *funct = (instruction & 0x3F);
+    *offset = (instruction & 0xFFFF);
+    *jsec = (instruction & 0x1FFFFFF);
 
 }
 
@@ -42,21 +57,128 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 /* 30 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
-
+    if(op == 0){ // R type control signals
+        controls->RegDst = '1';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = '0';
+        controls->ALUOp = '7';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '0';
+        controls->RegWrite = '1';
+        return 0;
+    }
+    else if(op = 35){ // Lw - Load word
+        controls->RegDst = '0';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '1';
+        controls->MemtoReg = '1';
+        controls->ALUOp = '0';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '1';
+        controls->RegWrite = '1';
+        return 0;
+    }
+    else if(op = 43){ // Sw - Store word
+        controls->RegDst = 'DC';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = 'DC';
+        controls->ALUOp = '0';
+        controls->MemWrite = '1';
+        controls->ALUSrc = '1';
+        controls->RegWrite = '0';
+        return 0;
+    }
+    else if(op == 4){ // Beq - Branch if equal
+        controls->RegDst = 'DC';
+        controls->Jump = '0';
+        controls->Branch = '1';
+        controls->MemRead = '0';
+        controls->MemtoReg = 'DC';
+        controls->ALUOp = '1';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '0';
+        controls->RegWrite = '0';
+        return 0;
+    }
+    else if(op == 10){ // Slti - Set if less than immediate
+        controls->RegDst = '0';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = '0';
+        controls->ALUOp = '2';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '1';
+        controls->RegWrite = '1';
+        return 0;
+    }
+    else if(op == 9){ // Sltiu - Set on less than immediate unsigned
+        controls->RegDst = '0';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = '0';
+        controls->ALUOp = '3';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '1';
+        controls->RegWrite = '1';
+        return 0;
+    }
+    else if(op == 8){ // Addi - Add immediate
+        controls->RegDst = '0';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = '0';
+        controls->ALUOp = '0';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '1';
+        controls->RegWrite = '1';
+        return 0;
+    }
+    else if(op == 67){ // Lui - Load upper immediate
+        controls->RegDst = '0';
+        controls->Jump = '0';
+        controls->Branch = '0';
+        controls->MemRead = '0';
+        controls->MemtoReg = '0';
+        controls->ALUOp = '6';
+        controls->MemWrite = '0';
+        controls->ALUSrc = '1';
+        controls->RegWrite = '1';
+        return 0;
+    }
+    else{ // Unknown Op
+        return 1;
+    }
 }
 
 /* Read Register */
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-
+    *data1 = Reg[r1];
+    *data2 = Reg[r2];
 }
 
 /* Sign Extend */
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-
+    if(offset >> 15 == 1){
+        *extended_value = (offset | 0xFFFF0000);
+    }
+    else if(offset >> 15 == 0){
+        *extended_value = offset;
+    }
+    else{
+        return;
+    }
 }
 
 /* ALU Operations */
@@ -99,13 +221,35 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
 /* 10 Points */
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
-
+    if(MemRead != 0){
+        if(ALUresult%4 == 0){
+            *memdata = ALUresult;
+        }
+        else{
+            return 1;
+        }
+    }
+    if(MemWrite != 0){
+        if(ALUresult%4 == 0){
+            ALUresult = data2;
+        }
+        else{
+            return 1;
+        }
+    }
+    return 0;
 }
 
 /* Write Register */
 /* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
+    if(RegWrite == 0){
+        return;
+    }
+    else if(RegWrite != 0){
+        if()
+    }
 
 }
 
